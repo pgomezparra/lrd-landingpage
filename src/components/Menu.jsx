965e4698f33328557665e1c0228eed175f6b79e8
Pedro Gomez <pandresgomezp@gmail.com>
2023-11-12 17:49:01 -0500

    Adjust in font, colors and footer

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,21 +3,30 @@ import {useState} from "react";
 import {Link} from "react-router-dom";
 export default function Menu() {
     const [openMenu, setOpenMenu] = useState(false)
-    const [activeMenu, setActiveMenu] = useState(0)
+
+    const key = "selectedMenu"
+    const selectedMenuStored = localStorage.getItem(key)
+    let actualMenu = 0
+    if (selectedMenuStored) {
+        actualMenu = parseInt(selectedMenuStored)
+    }
+    const [activeMenu, setActiveMenu] = useState(actualMenu)
     const changeOpenMenu = () => {
         setOpenMenu(!openMenu)
     }
     const closeMenu = (index) => {
         setOpenMenu(false)
         setActiveMenu(index)
+        localStorage.setItem(key, index)
     }
 
   return (
     <nav className="menu">
         <img className="menu__hamburguer-logo" onClick={changeOpenMenu} src={menuLogo} alt="menu" />
       <ul className={openMenu ? 'menu__list menu__open-menu' : 'menu__list menu__closed-menu'}>
-          <Link  to="/" style={{ textDecoration: 'none' }}><li onClick={() => closeMenu(0)} className={activeMenu == 0 ? 'menu-active' : ''}>Inicio</li></Link>
-          <Link  to="/institution" style={{ textDecoration: 'none' }}><li onClick={() => closeMenu(1)} className={activeMenu == 1 ? 'menu-active' : ''}>Institución</li></Link>
+          <Link  to="/" style={{ textDecoration: 'none' }}><li onClick={() => closeMenu(0)} className={activeMenu === 0 ? 'menu-active' : ''}>Inicio</li></Link>
+          <Link  to="/institution" style={{ textDecoration: 'none' }}><li onClick={() => closeMenu(1)} className={activeMenu === 1 ? 'menu-active' : ''}>Institución</li></Link>
+          <Link  to="https://classroom.google.com/" style={{ textDecoration: 'none' }} target="_blank"><li>Plataforma</li></Link>
           {/*<li onClick={closeMenu}><Link to="/institution" style={{ textDecoration: 'none' }}>Docentes</Link></li>*/}
           {/*<li onClick={closeMenu}><Link to="/institution" style={{ textDecoration: 'none' }}>Estudiantes</Link></li>*/}
           {/*<li onClick={closeMenu}><Link to="/institution" style={{ textDecoration: 'none' }}>Información</Link></li>*/}
