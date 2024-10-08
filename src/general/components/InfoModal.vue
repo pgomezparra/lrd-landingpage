f9597553829fdf429753229f959fdbf53cf73672
Pedro Andrés Gómez Parra <andresg@MacBook-Pro-de-Pedro.local>
2024-10-07 21:38:32 -0500

    Fix favicon + Hide main modal

--- src/general/components/InfoModal.vue
+++ src/general/components/InfoModal.vue
@@ -15,7 +15,7 @@ import { ref, onMounted } from 'vue'
 import confetti from 'canvas-confetti'
 
 // Modal state
-const isModalOpen = ref(true)
+const isModalOpen = ref(false)
 
 // Function to close the modal
 const closeModal = () => {
@@ -23,30 +23,30 @@ const closeModal = () => {
 }
 
 // Optional: Automatically show the modal when the page loads
-onMounted(() => {
-  isModalOpen.value = true
-
-  const duration = 2 * 1000
-  const animationEnd = Date.now() + duration
-  const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 10000 }
-
-  function randomInRange(min, max) {
-    return Math.random() * (max - min) + min
-  }
-
-  const interval = setInterval(function() {
-    const timeLeft = animationEnd - Date.now()
-
-    if (timeLeft <= 0) {
-      return clearInterval(interval)
-    }
-
-    const particleCount = 50 * (timeLeft / duration)
-    // since particles fall down, start a bit higher than random
-    confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } })
-    confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } })
-  }, 250)
-})
+// onMounted(() => {
+//   isModalOpen.value = true
+//
+//   const duration = 2 * 1000
+//   const animationEnd = Date.now() + duration
+//   const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 10000 }
+//
+//   function randomInRange(min, max) {
+//     return Math.random() * (max - min) + min
+//   }
+//
+//   const interval = setInterval(function() {
+//     const timeLeft = animationEnd - Date.now()
+//
+//     if (timeLeft <= 0) {
+//       return clearInterval(interval)
+//     }
+//
+//     const particleCount = 50 * (timeLeft / duration)
+//     // since particles fall down, start a bit higher than random
+//     confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } })
+//     confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } })
+//   }, 250)
+// })
 </script>
 
 <style scoped>
