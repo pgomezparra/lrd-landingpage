efc0c05318b550f6cd0c155fcc1e44ae35e4ee29
Pedro Gómez <83526980+pgomezparra@users.noreply.github.com>
2026-02-11 08:51:52 -0500

    Update admissions info

--- src/admissions/views/Admissions.vue
+++ src/admissions/views/Admissions.vue
@@ -1,5 +1,5 @@
 <template>
-  <Navbar />
+  <Navbar/>
   <div class="l-admissions">
     <main>
       <div class="l-admissions__container">
@@ -11,7 +11,8 @@
             <div class="o-admissions-card__content">
               <p class="o-admissions-card__subtitle">Requisitos para todos los estudiantes:</p>
               <ul class="o-admissions-card__list">
-                <li>Formulario de inscripción <b>($20.000 para nuevos y $5.000 para antiguos)</b></li>
+                <!--                <li>Formulario de inscripción <b>($20.000 para nuevos y $5.000 para antiguos)</b></li>-->
+                <li>Formulario de inscripción</li>
                 <li>Registro civil de nacimiento</li>
                 <li>Fotocopia de Tarjeta de Identidad (si aplica)</li>
                 <li>Fotocopia de la cédula de los padres o acudientes</li>
@@ -36,58 +37,58 @@
           </div>
         </section>
 
-        <section class="l-admissions__container__section">
-          <div class="o-admissions-card">
-            <h2 class="o-admissions-card__title">Costos por nivel educativo</h2>
-            <div class="o-admissions-card__content">
-              <div class="o-admissions-card__content__table-wrapper">
-                <table class="o-admissions-card__content__table-wrapper__table">
-                  <thead>
-                  <tr>
-                    <th>Nivel</th>
-                    <th>Matrícula</th>
-                    <th>Pensión mensual</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr>
-                    <td>Preescolar</td>
-                    <td>$270.000</td>
-                    <td>$125.000</td>
-                  </tr>
-                  <tr>
-                    <td>Primaria</td>
-                    <td>$270.000</td>
-                    <td>$125.000</td>
-                  </tr>
-                  <tr>
-                    <td>6º a 9º</td>
-                    <td>$300.000</td>
-                    <td>$140.000</td>
-                  </tr>
-                  <tr>
-                    <td>10º a 11º</td>
-                    <td>$310.000</td>
-                    <td>$145.000</td>
-                  </tr>
-                  </tbody>
-                </table>
-              </div>
-              <p class="o-admissions-card__subtitle">Incluye:</p>
-              <ul class="o-admissions-card__list">
-                <li>Matrícula</li>
-                <li>Seguro estudiantil (contra accidentes)</li>
-                <li>Carné estudiantil</li>
-                <li>Fotos</li>
-                <li>Sistematización y papelería</li>
-                <li>Agenda escolar</li>
-              </ul>
-              <p class="o-admissions-card__note">Nota: Los costos pueden estar sujetos a cambios. Por favor, contacte a
-                la administración
-                para obtener información actualizada.</p>
-            </div>
-          </div>
-        </section>
+        <!--        <section class="l-admissions__container__section">-->
+        <!--          <div class="o-admissions-card">-->
+        <!--            <h2 class="o-admissions-card__title">Costos por nivel educativo</h2>-->
+        <!--            <div class="o-admissions-card__content">-->
+        <!--              <div class="o-admissions-card__content__table-wrapper">-->
+        <!--                <table class="o-admissions-card__content__table-wrapper__table">-->
+        <!--                  <thead>-->
+        <!--                  <tr>-->
+        <!--                    <th>Nivel</th>-->
+        <!--                    <th>Matrícula</th>-->
+        <!--                    <th>Pensión mensual</th>-->
+        <!--                  </tr>-->
+        <!--                  </thead>-->
+        <!--                  <tbody>-->
+        <!--                  <tr>-->
+        <!--                    <td>Preescolar</td>-->
+        <!--                    <td>$270.000</td>-->
+        <!--                    <td>$125.000</td>-->
+        <!--                  </tr>-->
+        <!--                  <tr>-->
+        <!--                    <td>Primaria</td>-->
+        <!--                    <td>$270.000</td>-->
+        <!--                    <td>$125.000</td>-->
+        <!--                  </tr>-->
+        <!--                  <tr>-->
+        <!--                    <td>6º a 9º</td>-->
+        <!--                    <td>$300.000</td>-->
+        <!--                    <td>$140.000</td>-->
+        <!--                  </tr>-->
+        <!--                  <tr>-->
+        <!--                    <td>10º a 11º</td>-->
+        <!--                    <td>$310.000</td>-->
+        <!--                    <td>$145.000</td>-->
+        <!--                  </tr>-->
+        <!--                  </tbody>-->
+        <!--                </table>-->
+        <!--              </div>-->
+        <!--              <p class="o-admissions-card__subtitle">Incluye:</p>-->
+        <!--              <ul class="o-admissions-card__list">-->
+        <!--                <li>Matrícula</li>-->
+        <!--                <li>Seguro estudiantil (contra accidentes)</li>-->
+        <!--                <li>Carné estudiantil</li>-->
+        <!--                <li>Fotos</li>-->
+        <!--                <li>Sistematización y papelería</li>-->
+        <!--                <li>Agenda escolar</li>-->
+        <!--              </ul>-->
+        <!--              <p class="o-admissions-card__note">Nota: Los costos pueden estar sujetos a cambios. Por favor, contacte a-->
+        <!--                la administración-->
+        <!--                para obtener información actualizada.</p>-->
+        <!--            </div>-->
+        <!--          </div>-->
+        <!--        </section>-->
 
         <section class="l-admissions__container__section">
           <div class="o-admissions-card">
@@ -98,7 +99,8 @@
               <ul class="o-admissions-card__list">
                 <li>Teléfono: <a class="o-admissions-card__link"
                                  href="tel:+573158317079">3158317079</a> - <a class="o-admissions-card__link"
-                                 href="tel:+573158317079">3184010834</a></li>
+                                                                              href="tel:+573158317079">3184010834</a>
+                </li>
                 <li>Email: secretaria@liceoreydavid.edu.co</li>
                 <li>Horario de atención: Lunes a Viernes de 6:30 am - 12:00 pm</li>
               </ul>
@@ -107,9 +109,9 @@
         </section>
       </div>
     </main>
-    <Footer />
+    <Footer/>
   </div>
-  <whatsapp />
+  <whatsapp/>
 </template>
 
 <script setup>
